771ad0b8a0efec9c73209646d49627ac3e5dc9e8
Matthew <sorianomatthew@yahoo.com>
2022-01-22 18:58:00 -0500

    Basic HTTP Assignment done

--- src/server.js
+++ src/server.js
@@ -2,19 +2,22 @@ console.log("Hello World!");
 
 const _ = require("underscore");
 const http = require("http");
+const fs = require("fs");
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
 const onRequest = (request, response) => {
     if(request.url === "/favicon.ico"){
         console.log("favicon request");
     }
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
+    console.log(request.url);
 
-    console.log("request received");
+    response.writeHead(200, {"Content-Type": "text/html"});
 
-    response.write("Hello World!");
+    response.write(index);
 
     response.end();
 };
